f5d2c9219e8f58358a92b4b316df581aa7c54ad8
“Yangbo <2857981L@student.gla.ac.uk>
2026-02-11 10:46:00 +0000

    ch6: dynamic index + category page

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-    return render(request, 'rango/index.html')
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
+    context_dict = {}
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
+
+
+def show_category(request, category_name):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category).order_by('-views')
+
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
